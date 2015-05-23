1fca28daa01b17864f3df31d368005f8742eed56
bitcoinfees <bitcoinfees@gmail.com>
2015-05-23 14:03:59 +0800

    Add memblocktransfer to feemodel-tools

--- feemodeldata/tools/cli.py
+++ feemodeldata/tools/cli.py
@@ -18,3 +18,24 @@ def mempooldump(filename):
 def mempoolload(filename):
     from feemodeldata.tools.mempooldump import load
     load(filename)
+
+
+@cli.command()
+@click.argument("source", type=click.STRING, required=True)
+@click.argument("dest", type=click.STRING, required=True)
+@click.argument("startheight", type=click.INT, required=True)
+@click.argument("endheight", type=click.INT, required=True)
+def memblocktransfer(source, dest, startheight, endheight):
+    import sqlite3
+    from feemodel.txmempool import MemBlock
+    for height in range(startheight, endheight+1):
+        b = MemBlock.read(height)
+        if b:
+            try:
+                b.write(dest, float("inf"))
+            except sqlite3.IntegrityError:
+                click.echo("Height {} already exists in dest.".format(height))
+            else:
+                click.echo("Height {} written.".format(height))
+        else:
+            click.echo("Height {} missing.".format(height))
